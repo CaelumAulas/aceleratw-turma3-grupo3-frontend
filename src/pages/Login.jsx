7107ba865a6347a9a31a50119ccac9ea5bee1d26
David Fernandes <david.fernandes@MacBook-Pro-de-David.local>
2021-06-07 19:46:07 -0300

    add layout login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+
+import {
+    Container,
+    Grid,
+    Typography,
+    TextField,
+    Button
+} from '@material-ui/core';
+
+export function Login() {
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(username);
+        console.log(password);
+    }
+
+    return (
+        <Container maxWidth="xl">
+            <Grid
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justify="center"
+                style={{ minHeight: '100vh' }}
+            >
+                <Grid item xl={3}>
+                    <Typography variant="h5" component="h1" align="center">
+                        Login
+                    </Typography>
+                    <form onSubmit={handleSubmit}>
+                        <TextField
+                            type="text"
+                            id="username"
+                            label="Usuário"
+                            variant="outlined"
+                            margin="normal"
+                            onChange={(event) => setUsername(event.target.value)}
+                            fullWidth
+                        />
+                        <TextField
+                            type="password"
+                            id="password"
+                            label="Senha"
+                            variant="outlined"
+                            margin="normal"
+                            onChange={(event) => setPassword(event.target.value)}
+                            fullWidth
+                        />
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                            size="large"
+                            fullWidth
+                        >
+                            Logar
+                        </Button>
+                    </form>
+                </Grid>
+            </Grid>
+        </Container>
+    )
+}
