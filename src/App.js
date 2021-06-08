60228e1ccbf2438905b608ea888bbbfd4f2723d0
David <fernandesc.david@gmail.com>
2021-06-08 14:40:02 -0300

    remove home page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Home } from './pages/Home';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
 
+import { Login } from './pages/Login';
+
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/">
-          <Home />
+        <Route exact path="/sign-in">
+          <Login />
         </Route>
       </Switch>
     </Router>
