e6c0c184fc6d717509baa09e382cef25ee7f008c
Letícia Monteiro <leticiamonteiro@leticiamonteiro.local>
2021-06-17 21:18:24 -0300

    Set theme font face

--- src/App.js
+++ src/App.js
@@ -1,71 +1,89 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+import './App.css';
+
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect
+  Redirect,
 } from "react-router-dom";
 
-import { UserContext, UserProvider } from './contexts/user';
-import { MenuProvider } from './contexts/menu';
-import { ToolBar } from './components/ToolBar';
-import { Login } from './pages/Login';
-import { Dashboard } from './pages/Dashboard';
-import { UserForm } from './pages/UserForm';
-import { UserList } from './pages/UserList';
-import { VehicleForm } from './pages/VehicleForm';
-import { VehicleList } from './pages/VehicleList';
-import { BrandForm } from './pages/BrandForm';
-import { BrandList } from './pages/BrandList';
-import { NotFoundPage } from './pages/NotFoundPage';
+import { UserContext, UserProvider } from "./contexts/user";
+import { MenuProvider } from "./contexts/menu";
+import { ToolBar } from "./components/ToolBar";
+import { Login } from "./pages/Login";
+import { Dashboard } from "./pages/Dashboard";
+import { UserForm } from "./pages/UserForm";
+import { UserList } from "./pages/UserList";
+import { VehicleForm } from "./pages/VehicleForm";
+import { VehicleList } from "./pages/VehicleList";
+import { BrandForm } from "./pages/BrandForm";
+import { BrandList } from "./pages/BrandList";
+import { NotFoundPage } from "./pages/NotFoundPage";
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      "Nunito",
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+    ].join(","),
+  },
+});
 
 function App() {
   const { userIsLogged } = useContext(UserContext);
 
   return (
-    <UserProvider>
-      <MenuProvider>
-        <Router>
-          <ToolBar />
-          <Switch>
-            <Route exact path="/">
-              {
-                userIsLogged
-                  ? <Redirect to="/dashboard" />
-                  : <Redirect to="/sign-in" />
-              }
-            </Route>
-            <Route path="/sign-in">
-              <Login />
-            </Route>
-            <Route path="/dashboard">
-              <Dashboard />
-            </Route>
-            <Route path="/cadastro-usuario">
-              <UserForm />
-            </Route>
-            <Route path="/lista-usuarios">
-              <UserList />
-            </Route>
-            <Route path="/cadastro-veiculo">
-              <VehicleForm />
-            </Route>
-            <Route path="/lista-veiculos">
-              <VehicleList />
-            </Route>
-            <Route path="/cadastro-marca">
-              <BrandForm />
-            </Route>
-            <Route path="/lista-marcas">
-              <BrandList />
-            </Route>
-            <Route path="/**">
-              <NotFoundPage />
-            </Route>
-          </Switch>
-        </Router>
-      </MenuProvider>
-    </UserProvider>
+    <ThemeProvider theme={theme}>
+      <UserProvider>
+        <MenuProvider>
+          <Router>
+            <ToolBar />
+            <Switch>
+              <Route exact path="/">
+                {userIsLogged ? (
+                  <Redirect to="/dashboard" />
+                ) : (
+                  <Redirect to="/sign-in" />
+                )}
+              </Route>
+              <Route path="/sign-in">
+                <Login />
+              </Route>
+              <Route path="/dashboard">
+                <Dashboard />
+              </Route>
+              <Route path="/cadastro-usuario">
+                <UserForm />
+              </Route>
+              <Route path="/lista-usuarios">
+                <UserList />
+              </Route>
+              <Route path="/cadastro-veiculo">
+                <VehicleForm />
+              </Route>
+              <Route path="/lista-veiculos">
+                <VehicleList />
+              </Route>
+              <Route path="/cadastro-marca">
+                <BrandForm />
+              </Route>
+              <Route path="/lista-marcas">
+                <BrandList />
+              </Route>
+              <Route path="/**">
+                <NotFoundPage />
+              </Route>
+            </Switch>
+          </Router>
+        </MenuProvider>
+      </UserProvider>
+    </ThemeProvider>
   );
 }
 
